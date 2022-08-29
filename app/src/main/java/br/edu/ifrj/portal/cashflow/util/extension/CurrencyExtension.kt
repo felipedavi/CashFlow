dbb7f26e681eb563493eab36feeaa90e0005f29a
Felipe Oliveira <31748519+felipedavi@users.noreply.github.com>
2022-08-29 15:45:56 -0300

    feat: adicionando a extensão monetária CurrencyExtension.kt

---
+++ app/src/main/java/br/edu/ifrj/portal/cashflow/util/extension/CurrencyExtension.kt
@@ -0,0 +1,22 @@
+package br.edu.ifrj.portal.cashflow.util.extension
+
+import java.text.DecimalFormat
+import java.text.DecimalFormatSymbols
+import java.util.*
+
+private val onlyNumberRegex by lazy { "[^0-9 ]".toRegex() }
+private const val DECIMAL_FACTOR = 100
+private const val CURRENCY_PATTERN = "R$ #,###,##0.00"
+private val CURRENCY_PATTERN_BRAZIL: DecimalFormatSymbols = DecimalFormatSymbols(
+    Locale("pt", "BR")).also {
+    it.decimalSeparator = ','
+    it.groupingSeparator = '.'
+}
+
+fun String.fromCurrency(): Double = this
+    .replace(onlyNumberRegex, "")
+    .toDouble()
+    .div(DECIMAL_FACTOR)
+
+fun Double.toCurrency(): String = DecimalFormat(CURRENCY_PATTERN, CURRENCY_PATTERN_BRAZIL)
+    .format(this)
