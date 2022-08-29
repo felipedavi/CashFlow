fa7e0b9ee65bbcc8059bd2fd410b8eadf9c1ed7e
Felipe Oliveira <31748519+felipedavi@users.noreply.github.com>
2022-08-29 14:50:07 -0300

    corrigindo tudo de navegação e layout e um pouco de pacotes também

--- app/src/main/java/br/edu/ifrj/portal/cashflow/feature/transaction/add/ui/TransactionAddFragment.kt
+++ app/src/main/java/br/edu/ifrj/portal/cashflow/feature/transaction/add/ui/TransactionAddFragment.kt
@@ -1,19 +1,19 @@
-package br.edu.ifrj.portal.cashflow.feature.transaction.detail
+package br.edu.ifrj.portal.cashflow.feature.transaction.add.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import br.edu.ifrj.portal.cashflow.R
 
-class TransactionDetailFragment : Fragment() {
+class TransactionAddFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_transaction_detail, container, false)
+        return inflater.inflate(R.layout.fragment_transaction_add, container, false)
     }
 
 }
--- app/src/main/java/br/edu/ifrj/portal/cashflow/feature/transaction/detail/ui/TransactionDetailFragment.kt
+++ app/src/main/java/br/edu/ifrj/portal/cashflow/feature/transaction/detail/ui/TransactionDetailFragment.kt
@@ -1,19 +1,19 @@
-package br.edu.ifrj.portal.cashflow.feature.transaction.add
+package br.edu.ifrj.portal.cashflow.feature.transaction.detail.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import br.edu.ifrj.portal.cashflow.R
 
-class TransactionAddFragment : Fragment() {
+class TransactionDetailFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_transaction_add, container, false)
+        return inflater.inflate(R.layout.fragment_transaction_detail, container, false)
     }
 
 }
