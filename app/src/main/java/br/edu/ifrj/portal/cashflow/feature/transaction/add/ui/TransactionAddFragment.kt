e1f7fec5258fb7f60045eb422ccbe4e413c745a5
Felipe Oliveira <31748519+felipedavi@users.noreply.github.com>
2022-08-29 15:41:23 -0300

    chore: adicionando o suporte ao datepicker no TransactionAddFragment.kt

--- app/src/main/java/br/edu/ifrj/portal/cashflow/feature/transaction/add/ui/TransactionAddFragment.kt
+++ app/src/main/java/br/edu/ifrj/portal/cashflow/feature/transaction/add/ui/TransactionAddFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import br.edu.ifrj.portal.cashflow.databinding.FragmentTransactionAddBinding
+import br.edu.ifrj.portal.cashflow.util.DatePickerFragment
 
 class TransactionAddFragment : Fragment() {
     private var _binding: FragmentTransactionAddBinding? = null
@@ -16,12 +17,24 @@ class TransactionAddFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentTransactionAddBinding.inflate(inflater, container, false)
+        setListeners()
         return binding.root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun setListeners() {
+        binding.editDate.keyListener = null
+
+        binding.editDate.onFocusChangeListener = View.OnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                DatePickerFragment(binding.editDate.text.toString()) { binding.editDate.setText(it) }
+                    .show(requireActivity().supportFragmentManager, "datePicker")
+            }
+        }
     }
 
 }
