1997431c2c727a847beda3a0443c8df12a181520
Felipe Oliveira <31748519+felipedavi@users.noreply.github.com>
2022-08-29 14:50:57 -0300

    chore: suporte adicionado a navegação das telas

--- app/src/main/java/br/edu/ifrj/portal/cashflow/MainActivity.kt
+++ app/src/main/java/br/edu/ifrj/portal/cashflow/MainActivity.kt
@@ -1,15 +1,31 @@
 package br.edu.ifrj.portal.cashflow
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import br.edu.ifrj.portal.cashflow.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+
+        NavigationUI.setupActionBarWithNavController(this, navController)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
