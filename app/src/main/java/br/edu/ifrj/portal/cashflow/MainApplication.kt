e5893f6e81f37cc688fe8f6e0a61d8d3f7b6de89
Felipe Oliveira <31748519+felipedavi@users.noreply.github.com>
2022-08-29 18:25:14 -0300

    feat: atualização da classe application

--- app/src/main/java/br/edu/ifrj/portal/cashflow/MainApplication.kt
+++ app/src/main/java/br/edu/ifrj/portal/cashflow/MainApplication.kt
@@ -1,8 +1,13 @@
 package br.edu.ifrj.portal.cashflow
 
 import android.app.Application
+import br.edu.ifrj.portal.cashflow.base.TransactionDatabase
+import br.edu.ifrj.portal.cashflow.data.TransactionRepository
 
 class MainApplication: Application() {
+
+    private val database by lazy { TransactionDatabase.getDatabase(this@MainApplication) }
+    val repository by lazy { TransactionRepository(database.transactionDAO()) }
 
     override fun onCreate() {
         super.onCreate()
