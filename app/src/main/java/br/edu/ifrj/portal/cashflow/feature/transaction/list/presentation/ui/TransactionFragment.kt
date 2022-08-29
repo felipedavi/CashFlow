b587a121d79002781118173469b6afd35a269299
Felipe Oliveira <31748519+felipedavi@users.noreply.github.com>
2022-08-29 15:03:23 -0300

    chore: suporte adicionado a navegação para tela de cadastro

--- app/src/main/java/br/edu/ifrj/portal/cashflow/feature/transaction/list/presentation/ui/TransactionFragment.kt
+++ app/src/main/java/br/edu/ifrj/portal/cashflow/feature/transaction/list/presentation/ui/TransactionFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import br.edu.ifrj.portal.cashflow.R
 import br.edu.ifrj.portal.cashflow.databinding.FragmentTransactionBinding
 
 class TransactionFragment : Fragment() {
@@ -16,12 +18,19 @@ class TransactionFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentTransactionBinding.inflate(inflater, container, false)
+        setListeners()
         return binding.root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun setListeners() {
+        binding.floatingActionButton.setOnClickListener {
+            findNavController().navigate(R.id.navigateToTransactionAddFragment)
+        }
     }
 
 }
