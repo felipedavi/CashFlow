5dcfcdad334988f99259876ad295089289aec90b
Felipe Oliveira <31748519+felipedavi@users.noreply.github.com>
2022-08-29 17:40:40 -0300

    chore: validação edit text

--- app/src/main/java/br/edu/ifrj/portal/cashflow/util/extension/ViewExtension.kt
+++ app/src/main/java/br/edu/ifrj/portal/cashflow/util/extension/ViewExtension.kt
@@ -2,9 +2,12 @@ package br.edu.ifrj.portal.cashflow.util.extension
 
 import android.content.Context
 import android.os.Build
+import android.text.TextUtils
 import android.view.View
 import android.view.WindowInsets
 import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import br.edu.ifrj.portal.cashflow.R
 
 fun View.hideKeyboard() {
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
@@ -14,4 +17,15 @@ fun View.hideKeyboard() {
         inputMethodManager.hideSoftInputFromWindow(windowToken, 0)
     }
     this.clearFocus()
+}
+
+fun EditText.isValid(): Boolean {
+    val text = this.text.toString().trim()
+    return if (TextUtils.isEmpty(text)) {
+        this.error = resources.getString(R.string.edit_error)
+        false
+    } else {
+        this.error = null
+        true
+    }
 }
