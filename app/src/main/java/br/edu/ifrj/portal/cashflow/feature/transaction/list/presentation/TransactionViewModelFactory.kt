819fafd24944838e854c14edbea870841c0fb314
Felipe Oliveira <31748519+felipedavi@users.noreply.github.com>
2022-08-29 18:53:04 -0300

    feat: criação do ViewModel e ViewModelFactory da listagem de transações

---
+++ app/src/main/java/br/edu/ifrj/portal/cashflow/feature/transaction/list/presentation/TransactionViewModelFactory.kt
@@ -0,0 +1,14 @@
+package br.edu.ifrj.portal.cashflow.feature.transaction.list.presentation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import br.edu.ifrj.portal.cashflow.data.TransactionRepository
+
+class TransactionViewModelFactory(private val repository: TransactionRepository): ViewModelProvider.Factory {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(TransactionViewModel::class.java))
+            return TransactionViewModel(repository) as T
+        throw IllegalArgumentException("ViewModel Not Found")
+    }
+}
