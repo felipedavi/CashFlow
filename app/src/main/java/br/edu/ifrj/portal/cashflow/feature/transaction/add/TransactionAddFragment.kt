768cbaefa69925a86178cab02bc40ba183df8248
Felipe Oliveira <31748519+felipedavi@users.noreply.github.com>
2022-08-29 13:33:17 -0300

    feat: tela de cadastro e edição de transação

--- app/src/main/java/br/edu/ifrj/portal/cashflow/feature/transaction/add/TransactionAddFragment.kt
+++ app/src/main/java/br/edu/ifrj/portal/cashflow/feature/transaction/add/TransactionAddFragment.kt
@@ -1,18 +1,19 @@
-package br.edu.ifrj.portal.cashflow
+package br.edu.ifrj.portal.cashflow.feature.transaction.add
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import br.edu.ifrj.portal.cashflow.R
 
-class TransactionDetailFragment : Fragment() {
+class TransactionAddFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_transaction_detail, container, false)
+        return inflater.inflate(R.layout.fragment_transaction_add, container, false)
     }
 
 }
