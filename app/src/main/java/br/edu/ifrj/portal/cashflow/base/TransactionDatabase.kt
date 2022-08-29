ebb472f65a07fc9e460c87ad9b288eae32e307a0
Felipe Oliveira <31748519+felipedavi@users.noreply.github.com>
2022-08-29 18:17:08 -0300

    feat: criação do database TransactionDatabase.kt

---
+++ app/src/main/java/br/edu/ifrj/portal/cashflow/base/TransactionDatabase.kt
@@ -0,0 +1,29 @@
+package br.edu.ifrj.portal.cashflow.base
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import br.edu.ifrj.portal.cashflow.data.entity.TransactionEntity
+import br.edu.ifrj.portal.cashflow.data.local.TransactionDAO
+
+@Database(entities = [TransactionEntity::class], version = 1)
+abstract class TransactionDatabase : RoomDatabase() {
+    abstract fun transactionDAO() : TransactionDAO
+
+    companion object {
+        private var instance: TransactionDatabase? = null
+
+        fun getDatabase(context: Context): TransactionDatabase =
+            instance ?: synchronized(this) {
+                instance ?: buildDatabase(context).also {
+                    instance = it
+                }
+            }
+
+        private fun buildDatabase(context: Context) = Room.databaseBuilder(context,
+            TransactionDatabase::class.java, "transaction_database")
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+}
