d8fd7bcd8dcbae59bba8d201ff926b35859cb022
Felipe Oliveira <31748519+felipedavi@users.noreply.github.com>
2022-09-06 11:20:47 -0300

    chore: alterando o tipo do atributo data na entidade transação

--- app/src/main/java/br/edu/ifrj/portal/cashflow/data/entity/TransactionEntity.kt
+++ app/src/main/java/br/edu/ifrj/portal/cashflow/data/entity/TransactionEntity.kt
@@ -3,12 +3,13 @@ package br.edu.ifrj.portal.cashflow.data.entity
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import org.threeten.bp.OffsetDateTime
 
 @Entity(tableName = "transaction_table")
 data class TransactionEntity(
     @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "transaction_id") val id: Int = 0,
     var description: String,
-    var date: String,
+    var date: OffsetDateTime?= null,
     @ColumnInfo(name = "monetary_value") var monetaryValue: Double,
     @ColumnInfo(name = "transaction_type") var transactionType: Boolean
 )
