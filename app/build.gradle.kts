6a92be400c4bf42cbd7d94917227a8802d3d1e55
Felipe Oliveira <31748519+felipedavi@users.noreply.github.com>
2024-09-01 12:10:14 -0300

    refactor[deps]: Migra implementação de androidx.core:core-ktx para catálogos de versões

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,7 @@ android {
 dependencies {
 
     implementation(libs.androidx.ktx)
-    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation(libs.androidx.appcompat)
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     // Navigation Component
