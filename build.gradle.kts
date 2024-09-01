682639253c45672619e90c73acae36145172731b
Felipe Oliveira <31748519+felipedavi@users.noreply.github.com>
2024-09-01 12:37:25 -0300

    refactor[build]: Substitui id("com.android.application") por alias(libs.plugins.android.application) no build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.application") version Versions.AGP_VERSION apply false
+    alias(libs.plugins.android.application) apply false
     id("org.jetbrains.kotlin.android") version Versions.KOTLIN_VERSION apply false
     id("androidx.navigation.safeargs") version Versions.NAVIGATION_VERSION apply false
 }
