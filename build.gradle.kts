432d1d9ee589f37c2d46e813b25e1e2b29359e19
Felipe Oliveira <31748519+felipedavi@users.noreply.github.com>
2024-09-04 05:52:21 -0300

    build[deps]: Adiciona plugin do room no build.gradle (:project)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.androidx.room) apply false
     alias(libs.plugins.ksp) apply false
     id("androidx.navigation.safeargs") version Versions.NAVIGATION_VERSION apply false
 }
