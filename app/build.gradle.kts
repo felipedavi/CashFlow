d9de2e96c78e2f6114fd3d5e963fb322837476dd
Felipe Oliveira <31748519+felipedavi@users.noreply.github.com>
2024-09-04 05:44:29 -0300

    refactor[build]: Melhora legibilidade reorganizando o build.gradle.kts (:app)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,36 +23,46 @@ android {
             proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
     buildFeatures {
         viewBinding = true
     }
 }
 
 dependencies {
-
+    // AndroidX Core Components
     implementation(libs.androidx.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.material)
     implementation(libs.androidx.constraintlayout)
+
+    // Material Design
+    implementation(libs.material)
+
     // Navigation Component
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.navigation.ui.ktx)
+
     // Coroutines
     implementation(libs.kotlinx.coroutines.core)
+
     // Room components
     implementation(libs.androidx.room.ktx)
     annotationProcessor(libs.androidx.room.compiler)
     ksp(libs.androidx.room.compiler)
+
     // Lifecycle components
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.lifecycle.livedata.ktx)
+
     // Backport Android ThreeTen
     implementation(libs.threetenabp)
 }
