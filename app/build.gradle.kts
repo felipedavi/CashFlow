443786e7f6bb97aad4c52703e5678a2255c4d5fa
Felipe Oliveira <31748519+felipedavi@users.noreply.github.com>
2025-08-02 19:10:53 -0300

    build: atualiza versionCode para 4 e versionName para 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "meimaonamassa.cashflow"
         minSdk = 21
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.0.0"
+        versionCode = 4
+        versionName = "1.1.0"
     }
 
     buildTypes {
