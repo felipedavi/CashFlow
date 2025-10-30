2088539418ee145a1e91ed65a16633c28966671c
Felipe Davi <31748519+felipedavi@users.noreply.github.com>
2025-10-30 15:03:09 -0300

    refactor: usar Locale.forLanguageTag para instanciar o locale pt-BR

--- app/src/main/java/meimaonamassa/cashflow/util/extension/CurrencyExtension.kt
+++ app/src/main/java/meimaonamassa/cashflow/util/extension/CurrencyExtension.kt
@@ -8,7 +8,8 @@ private val onlyNumberRegex by lazy { "[^0-9 ]".toRegex() }
 private const val DECIMAL_FACTOR = 100
 private const val CURRENCY_PATTERN = "R$ #,###,##0.00"
 private val CURRENCY_PATTERN_BRAZIL: DecimalFormatSymbols = DecimalFormatSymbols(
-    Locale("pt", "BR")).also {
+    Locale.forLanguageTag("pt-BR")
+).also {
     it.decimalSeparator = ','
     it.groupingSeparator = '.'
 }
