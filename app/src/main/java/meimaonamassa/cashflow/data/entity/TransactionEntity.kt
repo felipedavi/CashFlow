2b28799402bf4977bdaf633b8d8f3724b00130ed
Felipe Davi <31748519+felipedavi@users.noreply.github.com>
2026-03-15 23:50:16 -0300

    refactor: reordenar propriedades na TransactionEntity
    A ordem das propriedades `description` e `payerPayee` foi alterada na definição da data class `TransactionEntity`.

--- app/src/main/java/meimaonamassa/cashflow/data/entity/TransactionEntity.kt
+++ app/src/main/java/meimaonamassa/cashflow/data/entity/TransactionEntity.kt
@@ -8,8 +8,8 @@ import org.threeten.bp.OffsetDateTime
 @Entity(tableName = "transaction_table")
 data class TransactionEntity(
     @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "transaction_id") val id: Int = 0,
-    var description: String,
     var payerPayee: String,
+    var description: String,
     var date: OffsetDateTime?= null,
     @ColumnInfo(name = "monetary_value") var monetaryValue: Double,
     @ColumnInfo(name = "transaction_type") var transactionType: Boolean
