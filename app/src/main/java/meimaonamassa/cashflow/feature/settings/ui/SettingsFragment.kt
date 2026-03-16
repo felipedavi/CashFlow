a27f2ae1de68bb38c06215a2502ad231545d5f44
Felipe Davi <31748519+felipedavi@users.noreply.github.com>
2026-03-15 23:02:00 -0300

    feat: criar SettingsFragment e seu respectivo layout
    Foram criados os arquivos `SettingsFragment.kt` e `fragment_settings.xml` para implementar a tela de configurações, utilizando View Binding para inflar o layout.

---
+++ app/src/main/java/meimaonamassa/cashflow/feature/settings/ui/SettingsFragment.kt
@@ -0,0 +1,29 @@
+package meimaonamassa.cashflow.feature.settings.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import meimaonamassa.cashflow.databinding.FragmentSettingsBinding
+
+
+class SettingsFragment : Fragment() {
+    private var _binding: FragmentSettingsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
