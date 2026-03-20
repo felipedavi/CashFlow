8f51d6be1d2ba7a098c58d4b1e638eb3ee70bc74
Felipe Davi <31748519+felipedavi@users.noreply.github.com>
2026-03-20 06:46:47 -0300

    refactor: garantir persistência da inserção de transação no TransactionAddViewModel
    - A chamada ao repositório para inserir uma transação foi envolvida em `withContext(Dispatchers.IO + NonCancellable)`.
    - O uso de `Dispatchers.IO` garante que a operação de banco de dados ocorra na thread apropriada, enquanto o `NonCancellable` assegura que a gravação seja concluída mesmo se o `viewModelScope` for cancelado durante a execução.

--- app/src/main/java/meimaonamassa/cashflow/feature/transaction/add/TransactionAddViewModel.kt
+++ app/src/main/java/meimaonamassa/cashflow/feature/transaction/add/TransactionAddViewModel.kt
@@ -2,14 +2,19 @@ package meimaonamassa.cashflow.feature.transaction.add
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.NonCancellable
 import meimaonamassa.cashflow.data.TransactionRepository
 import meimaonamassa.cashflow.data.entity.TransactionEntity
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class TransactionAddViewModel(private val repository: TransactionRepository) : ViewModel() {
     fun insert(transaction: TransactionEntity) {
         viewModelScope.launch {
-            repository.insert(transaction)
+            withContext(Dispatchers.IO + NonCancellable) {
+                repository.insert(transaction)
+            }
         }
     }
 }
